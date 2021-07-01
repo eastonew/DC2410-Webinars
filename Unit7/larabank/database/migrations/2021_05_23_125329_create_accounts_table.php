2eb0277cc6270dd06e1c0929dea41923a290680c
Eddie Easton <eastonew@aston.ac.uk>
2021-07-01 09:46:17 +0100

    Add unit 7 work

---
+++ Unit7/larabank/database/migrations/2021_05_23_125329_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('userid')->unsigned();
+            $table->timestamps();
+            $table->string('accountno');
+            $table->string('type');
+            $table->decimal('balance', 5, 2);
+            $table->decimal('interest', 5, 2);
+            $table->foreign('userid')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
---
+++ Unit7/larabank/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/hello', function () {
+    return "Hello World!!";
+});
+//Unit 6 - Routes
+Route::get('list', 'App\Http\Controllers\AccountController@list');
+Route::get('show/{id}', 'App\Http\Controllers\AccountController@show');
+//Auto added auth routes
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('display',[App\Http\Controllers\AccountController::class, 'display'])->name('display_account');
+//Route::get('display','App\Http\Controllers\AccountController@display')->name('display_account');
